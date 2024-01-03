e56f9e1c9366103dd22647eeb5eedb9b15eea86d
Christian Hoes <christian.hoes@hotmail.com>
2024-01-03 20:39:13 +0100

    First lines of codes and added todos

--- helloWorldApp/Program.cs
+++ helloWorldApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// TODO- Prio #1 in ein Repro - zB auf Git speichern um den code zu sichern und auch für jemand anderen zugänglich zu machen.
+
+int age = 0;
+string name = "";
+
+string namesAbfrage = "Wie ist Ihr Name? : ";
+string altersAbfrage = "Wie ist Ihr alter? : ";
+
+Console.WriteLine("Wilkommen zu unserem kleinen Tool!"); // TODO - einen Zeilenumbruch einfügen.
+Console.WriteLine(namesAbfrage);
+Console.ReadLine(); // TODO - Abfrage speichern.
+
+Console.WriteLine(altersAbfrage);
+Console.ReadLine();
+// TODO - Abfrage wieder am Bildschirm ausgeben.
+// TODO - Nach dem Alter fragen.
+// TODO - Das Alter ausgeben.
+
+// TODO - Dem User seine Daten wieder anzeigen und sich dafür bedanken und Fragen ob diese Daten so korrekt sind.
+
+// TODO - wenn diese Daten ok sind, dann eine Ausgabe am Schirm machen, dass das alles gespeichert wurde und das Fenster geschlossen werden kann.
+// TODO - wenn der User meint die Daten sind nicht korrekt, dann um eine erneute Eingabe aller Daten bitten.
