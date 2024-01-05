9bb8c4d3f3b840664b50571c009e4db0d624a1f7
OSM <mischa.osterberger@outlook.com>
2024-01-05 18:24:57 +0100

    ∆ refactored code √ added methods √ added comments

--- helloWorldApp/Program.cs
+++ helloWorldApp/Program.cs
@@ -1,51 +1,38 @@
-﻿string age = "";
-string name = "";
-string areTheDataOk = "";
+﻿#region variables and constants -> Gruppierung
 
+string age = string.Empty; // ist für mich besser lesbar (aber nicht zwingend!) als "" - denn es kann schnell ein Leerzeichen reinrutschen => sting xy = " ";
+string name = string.Empty;
+string areTheDataOk = string.Empty;
 
-string namesAbfrage = "Wie ist Ihr Name? : ";
-string altersAbfrage = "Wie ist Ihr alter? : ";
+const string namesAbfrage = "Wie ist Ihr Name? : "; // ich möchte nicht, dass das irgendwo im code abgeandert wird => CONST
+const string altersAbfrage = "Wie ist Ihr alter? : ";
+const string welcomeMessage = "Wilkommen zu unserem kleinen Tool!\n";
+const string endMessage = "\n\nIhre Eingaben wurden gespeichert und Sie können das Fenster nun schließen...";
+const string yes = "ja";
 
-Console.WriteLine("Wilkommen zu unserem kleinen Tool!\n");
+#endregion
 
-Console.WriteLine(namesAbfrage);
-name = Console.ReadLine(); 
+Console.WriteLine(welcomeMessage);
 
-Console.WriteLine(altersAbfrage);
-age = Console.ReadLine();
+GetNameAndAgeFromUser(out age, out name, out areTheDataOk, namesAbfrage, altersAbfrage);
 
-Console.WriteLine(age);
-
-Console.WriteLine($"Ist ihr Name? {name}  Ist Ihr Alter? {age} , sind diese Daten richtig? \n\n Bitte mit Ja und Nein beantworten!");
-areTheDataOk =  Console.ReadLine();
-
-
-if(areTheDataOk == "Ja") 
-{
-    Console.WriteLine("Ihre Eingaben wurden gespeichert und Sie können das Fenster nun schließen...");
-}
-else
+while (areTheDataOk.ToLower() != yes) // dann ist die Antwort immer "ja"  egal ob JA, Ja oder jA // das Tool soll so lange laufen bis die Antwort ja ist
 {
     Console.WriteLine("Bitte geben Sie ihre Daten nochmals ein.\n\n");
+    GetNameAndAgeFromUser(out age, out name, out areTheDataOk, namesAbfrage, altersAbfrage);
+}
 
+Console.WriteLine(endMessage);
+Console.ReadLine(); // sonst schließt sich das Fenster automatisch - wir wollen, dass die letzte Zeile sichtbar bleibt und das Fenster nicht automatisch geschlossen wird.
+
+static void GetNameAndAgeFromUser(out string age, out string name, out string areTheDataOk, string namesAbfrage, string altersAbfrage)
+{
     Console.WriteLine(namesAbfrage);
-    name = Console.ReadLine(); 
+    name = Console.ReadLine();
 
     Console.WriteLine(altersAbfrage);
     age = Console.ReadLine();
 
-    Console.WriteLine(age);
-
-    Console.WriteLine($"Ist ihr Name? {name}  Ist Ihr Alter? {age} , sind diese Daten richtig? \n\n Bitte mit Ja und Nein beantworten!");
-    areTheDataOk =  Console.ReadLine();
-
-}
-
-
-
-
-
-
-// TODO - logical Operaters anschauen
-// TODO - refactor - doppelter code
-// TODO - wieder zur Abfrage zurück // nach dem else-zweig
+    Console.WriteLine($"\n\nIst ihr Name => {name}  und Ihr Alter => {age} korrekt erfasst? \n\n => Bitte mit 'Ja' bzw. 'Nein' beantworten!\n\n");
+    areTheDataOk = Console.ReadLine();
+} // den doppelten Code nur einmal geschrieben und 2x verwendet. Vorteil - wenn etwas geaendert werden muss, dann muss das nur hier geaendert werden. Also in der METHODE
