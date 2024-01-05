972298a2db51174e7dd618e61ab3c19073928aa9
Christian Hoes <christian.hoes@hotmail.com>
2024-01-05 20:33:31 +0100

    Code refactored,

--- helloWorldApp/Program.cs
+++ helloWorldApp/Program.cs
@@ -2,50 +2,36 @@
 string name = "";
 string areTheDataOk = "";
 
-
 string namesAbfrage = "Wie ist Ihr Name? : ";
 string altersAbfrage = "Wie ist Ihr alter? : ";
 
+const string answerYes = "ja";
+
+
+// Anfang
 Console.WriteLine("Wilkommen zu unserem kleinen Tool!\n");
 
-Console.WriteLine(namesAbfrage);
-name = Console.ReadLine(); 
+GetNameAndAgeFromTheUser(out age, out name, out areTheDataOk, namesAbfrage, altersAbfrage);
 
-Console.WriteLine(altersAbfrage);
-age = Console.ReadLine();
-
-Console.WriteLine(age);
-
-Console.WriteLine($"Ist ihr Name? {name}  Ist Ihr Alter? {age} , sind diese Daten richtig? \n\n Bitte mit Ja und Nein beantworten!");
-areTheDataOk =  Console.ReadLine();
-
-
-if(areTheDataOk == "Ja") 
-{
-    Console.WriteLine("Ihre Eingaben wurden gespeichert und Sie können das Fenster nun schließen...");
-}
-else
+while (areTheDataOk?.ToLower() != answerYes)  
 {
     Console.WriteLine("Bitte geben Sie ihre Daten nochmals ein.\n\n");
 
+    GetNameAndAgeFromTheUser(out age, out name, out areTheDataOk, namesAbfrage, altersAbfrage);
+}
+
+Console.WriteLine("Ihre Eingaben wurden gespeichert und Sie können das Fenster nun schließen...");
+Console.ReadLine();
+// ENDE
+
+static void GetNameAndAgeFromTheUser(out string age, out string name, out string areTheDataOk, string namesAbfrage, string altersAbfrage)
+{
     Console.WriteLine(namesAbfrage);
     name = Console.ReadLine(); 
 
     Console.WriteLine(altersAbfrage);
     age = Console.ReadLine();
 
-    Console.WriteLine(age);
-
     Console.WriteLine($"Ist ihr Name? {name}  Ist Ihr Alter? {age} , sind diese Daten richtig? \n\n Bitte mit Ja und Nein beantworten!");
     areTheDataOk =  Console.ReadLine();
-
 }
-
-
-
-
-
-
-// TODO - logical Operaters anschauen
-// TODO - refactor - doppelter code
-// TODO - wieder zur Abfrage zurück // nach dem else-zweig
